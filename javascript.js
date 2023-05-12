57887f658ba49f8bbb8d139f923d600f00f4f29a
Yontemoon <monteyoon1998@gmail.com>
2023-05-12 11:07:45 -0700

    Decimals can be used, and calculations are rounded to the 2nd decimal.

--- javascript.js
+++ javascript.js
@@ -2,37 +2,47 @@
 const butOperators = document.querySelector('#butOperators');
 const showDisplay = document.querySelector('#showDisplay');
 const butNumbers = document.querySelector('#butNumbers');
-const butEqual = document.querySelector('#butEqual')
+const butEqual = document.querySelector('#butEqual');
+const butClear = document.querySelector('#butClear');
+
 let num1 = 0;
-let firstNum = true;
+let isFirstName = true;
 let num2 = 0;
 let getEqualNum = 0;
 let operator = "";
 
 function add() {
-    getEqualNum = Number(num1) + Number(num2);
+    getEqualNum = Math.round((Number(num1) + Number(num2))*100)/100;
     showDisplay.innerHTML = getEqualNum;
+    num1 = getEqualNum;
     return getEqualNum
 }
 
 function subtract() {
-    getEqualNum = Number(num1) - Number(num2);
+    getEqualNum = Math.round(Number(num1) - Number(num2)*100)/100;
     showDisplay.innerHTML = getEqualNum;
+    num1 = getEqualNum;
     return getEqualNum;
 }
 
 function multiply() {
-    getEqualNum = Number(num1) * Number(num2);
+    getEqualNum = Math.round(Number(num1) * Number(num2)*100)/100;
     showDisplay.innerHTML = getEqualNum;
+    num1 = getEqualNum;
     return getEqualNum;
 } 
 
 function divide() {
-    getEqualNum = Number(num1) / Number(num2);
+    getEqualNum = Math.round(Number(num1) / Number(num2)*100)/100;
     showDisplay.innerHTML = getEqualNum;
+    if (num2 ===  "0") {
+        showDisplay.innerHTML = "DIVIDE BY 0 ERROR";
+    }
+    num1 = getEqualNum;
     return getEqualNum;
 }
 
+// Do you really need? 
 function operate(num1, num2, operator) {
 
 }
@@ -42,17 +52,26 @@ function operate(num1, num2, operator) {
 
 butNumbers.addEventListener('click', (e) => {
     if (operator === "") {
-        if (firstNum === true) {
+        if (isFirstName === true) {
            num1 = showDisplay.innerHTML += e.target.innerHTML;
         } 
     } 
-    if (operator === "+" || operator === "*" ||  operator === "-" ||  operator === `/`){
-        firstNum = false;
-        num2 = showDisplay.innerHTML += e.target.innerHTML;
-        num2 = num2.replace("+", "");
-        num2 = num2.replace("-", "");
-        num2 = num2.replace("*", "");
-        num2 = num2.replace("/", "");
+    if (operator === "+" || 
+        operator === "*" ||  
+        operator === "-" ||  
+        operator === `/`) {
+            if (showDisplay.innerHTML === "+" || 
+                showDisplay.innerHTML === "*" ||  
+                showDisplay.innerHTML === "-" ||  
+                showDisplay.innerHTML === `/`) {
+                    showDisplay.innerHTML = "";
+                };
+            isFirstName = false;
+            num2 = showDisplay.innerHTML += e.target.innerHTML;
+            num2 = num2.replace("+", "");
+            num2 = num2.replace("-", "");
+            num2 = num2.replace("*", "");
+            num2 = num2.replace("/", "");
     }
 });
 
@@ -76,3 +95,11 @@ butEqual.addEventListener('click', () => {
 })
 
 
+butClear.addEventListener('click', () => {
+    num1 = 0;
+    isFirstName = true;
+    num2 = 0;
+    getEqualNum = 0;
+    operator = "";
+    showDisplay.innerHTML = "";
+})
