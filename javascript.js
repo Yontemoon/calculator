0f7de94efec0ab2eb7295e69bd287060cf824b4a
Yontemoon <monteyoon1998@gmail.com>
2023-05-12 10:17:42 -0700

    Equal sign is now functioning and working!

--- javascript.js
+++ javascript.js
@@ -6,22 +6,31 @@ const butEqual = document.querySelector('#butEqual')
 let num1 = 0;
 let firstNum = true;
 let num2 = 0;
+let getEqualNum = 0;
 let operator = "";
 
-function add(num1, num2) {
-    return num1 + num2;
+function add() {
+    getEqualNum = Number(num1) + Number(num2);
+    showDisplay.innerHTML = getEqualNum;
+    return getEqualNum
 }
 
-function subtract(num1, num2) {
-    return num1 - num2;
+function subtract() {
+    getEqualNum = Number(num1) - Number(num2);
+    showDisplay.innerHTML = getEqualNum;
+    return getEqualNum;
 }
 
-function multiply(num1, num2) {
-    return num1 * num2;
+function multiply() {
+    getEqualNum = Number(num1) * Number(num2);
+    showDisplay.innerHTML = getEqualNum;
+    return getEqualNum;
 } 
 
-function divide(num1, num2) {
-    return num1 / num2;
+function divide() {
+    getEqualNum = Number(num1) / Number(num2);
+    showDisplay.innerHTML = getEqualNum;
+    return getEqualNum;
 }
 
 function operate(num1, num2, operator) {
@@ -31,33 +40,39 @@ function operate(num1, num2, operator) {
 
 
 
-    butNumbers.addEventListener('click', (e) => {
-        if (operator === "") {
-            if (firstNum === true) {
-                num1 = showDisplay.innerHTML += e.target.innerHTML;
-            } 
+butNumbers.addEventListener('click', (e) => {
+    if (operator === "") {
+        if (firstNum === true) {
+           num1 = showDisplay.innerHTML += e.target.innerHTML;
         } 
-        if (operator === "+" || operator === "*" ||  operator === "-" ||  operator === `/`){
-            firstNum = false;
-            num2 = showDisplay.innerHTML += e.target.innerHTML;
-            num2 = num2.replace("+", "");
-            num2 = num2.replace("-", "");
-            num2 = num2.replace("*", "");
-            num2 = num2.replace("/", "");
-        }
-    });
+    } 
+    if (operator === "+" || operator === "*" ||  operator === "-" ||  operator === `/`){
+        firstNum = false;
+        num2 = showDisplay.innerHTML += e.target.innerHTML;
+        num2 = num2.replace("+", "");
+        num2 = num2.replace("-", "");
+        num2 = num2.replace("*", "");
+        num2 = num2.replace("/", "");
+    }
+});
 
 
-butOperators.addEventListener('click', function(e) {
+butOperators.addEventListener('click', (e) => {
     operator = showDisplay.innerHTML = e.target.innerHTML;
-    console.log(typeof operator);
+})
 
-    // num2Button();
+butEqual.addEventListener('click', () => {
+    if (operator === "+") {
+        add(num1, num2);
+    } else if (operator === "-") {
+        subtract(num1, num2);
+    } else if (operator === "*") {
+        multiply(num1, num2);
+    } else if (operator === "/") {
+        divide(num1, num2);
+    } else {
+     // Something.   
+    }
 })
 
 
-// function num2Button() {
-//     butNumbers.addEventListener('click', (f) => {
-//         num2 = showDisplay.innerHTML += f.target.innerHTML;
-//     });
-// }
